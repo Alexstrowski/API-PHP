76603069b5a48dbb2b4bbed2bd3831a383b33982
Alex <alex.angeles2694@gmail.com>
2018-09-23 12:31:39 -0500

    Actualizado usuario_asignatura

--- usuario_asignatura.php
+++ usuario_asignatura.php
@@ -1,11 +1,15 @@
 <?php
 	include 'Conexion.php';
 	ini_set('mssql.charset', 'UTF-8');
+	header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    
 	$conexion = new Conexion();
 	$cnn = $conexion->getConexion();
     
     if(isset($_GET["id_usuario"])){
-        $sql = "select a.id_asignatura,a2.nombre,a.porcentaje from usuario_asignatura as a inner join asignatura a2 on a.id_asignatura = a2.id_asignatura where a.id_usuario=?;";
+        $sql = "select a.id_asignatura,a2.nombre,a.porcentaje,a2.imagen from usuario_asignatura as a inner join asignatura a2 on a.id_asignatura = a2.id_asignatura where a.id_usuario=?;";
         $id_usuario=$_GET["id_usuario"];
         $resultado=$cnn->prepare($sql);
         $resultado->execute(array($id_usuario));
